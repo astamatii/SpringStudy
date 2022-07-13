5e495b1010bfcd039806ab7f612a4e6d0c57571c
Alexandr Stamatii <stam.sasha@gmail.com>
2022-07-13 16:52:09 +0300

    Learn GET request with parameters

--- SpringMVC_Annotation/src/main/java/com/astamatii/spring/springmvc_annotation/controllers/FirstController.java
+++ SpringMVC_Annotation/src/main/java/com/astamatii/spring/springmvc_annotation/controllers/FirstController.java
@@ -1,20 +1,29 @@
 package com.astamatii.spring.springmvc_annotation.controllers;
 
+import javax.servlet.http.HttpServletRequest;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class FirstController {
 
 	//Transfer
 	@GetMapping("/")
-	public String homepage() {
+	public String homepage(HttpServletRequest request) {
+		String name = request.getParameter("name");
+		String surname = request.getParameter("surname");
 		//name of the page by the template name
+		
+		System.out.println("Hello, " + name + " " + surname); 
 		return "first/homepage";
 	}
 	
 	@GetMapping("/1")
-	public String firstpage() {
+	public String firstpage(@RequestParam(value = "name", required = false) String name) {
+		
+		System.out.println("Hello, " + name);
 		return "first/firstpage";
 	}
 	
