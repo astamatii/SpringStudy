ed06c8997eb442df585bc5ca58e2a0718107296e
Alexandr Stamatii <stam.sasha@gmail.com>
2022-07-14 14:20:46 +0300

    Adding model and retrieving data from model

--- SpringMVC_Annotation/src/main/java/com/astamatii/spring/springmvc_annotation/controllers/FirstController.java
+++ SpringMVC_Annotation/src/main/java/com/astamatii/spring/springmvc_annotation/controllers/FirstController.java
@@ -3,6 +3,7 @@ package com.astamatii.spring.springmvc_annotation.controllers;
 import javax.servlet.http.HttpServletRequest;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
@@ -11,19 +12,26 @@ public class FirstController {
 
 	//Transfer
 	@GetMapping("/")
-	public String homepage(HttpServletRequest request) {
+	public String homepage(HttpServletRequest request, Model model) {
 		String name = request.getParameter("name");
 		String surname = request.getParameter("surname");
-		//name of the page by the template name
 		
-		System.out.println("Hello, " + name + " " + surname); 
+		if(name == null) name = "Muchacho";
+		if(surname == null) surname = "Amigo";
+		
+		//name of the page by the template name
+		model.addAttribute("message", "Hello " + name + " " + surname + "!");
+		
 		return "first/homepage";
 	}
 	
 	@GetMapping("/1")
-	public String firstpage(@RequestParam(value = "name", required = false) String name) {
+	public String firstpage(@RequestParam(value = "name", required = false) String name,
+			Model model) {
 		
-		System.out.println("Hello, " + name);
+		if(name == null) name = "Muchacho";
+		model.addAttribute("message", "Hello " + name + "!");
+		
 		return "first/firstpage";
 	}
 	
--- SpringMVC_Annotation/src/main/java/com/astamatii/spring/springmvc_annotation/controllers/SecondController.java
+++ SpringMVC_Annotation/src/main/java/com/astamatii/spring/springmvc_annotation/controllers/SecondController.java
@@ -1,8 +1,10 @@
 package com.astamatii.spring.springmvc_annotation.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 @RequestMapping("second")
@@ -13,4 +15,37 @@ public class SecondController {
 		return "second/about";
 	}
 	
+	@GetMapping("/calc")
+	public String calculator(@RequestParam("a") int a, 
+			@RequestParam("b") int b, 
+			@RequestParam("action") String action,
+			Model model) {
+		
+		double result;
+		
+		switch(action) {
+			case "*":
+				result = a * b;
+				break;
+			case "/":
+				result = a / (double)b;
+				break;
+			case "+":
+				result = a + b;
+				break;
+			case "-":
+				result = a - b;
+				break;
+			default:
+				result = 0;
+				break;
+		}
+		
+		model.addAttribute("result", result);
+		model.addAttribute("a", a);
+		model.addAttribute("b", b);
+		model.addAttribute("action", action);
+		
+		return "second/calculator";
+	}
 }
